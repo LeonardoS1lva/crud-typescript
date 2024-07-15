06e37d79f95b337ad61f00a5fd4c39f218a2b566
Leonardo Silva <109092809+LeonardoS1lva@users.noreply.github.com>
2024-07-15 01:59:48 +0000

    Adicionando estilos ao código

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,6 @@
 "use strict";
 const formularioTarefa = document.getElementById('form');
 const inputTarefa = document.getElementById('task');
-const btnVisualizar = document.getElementById('visualizar');
 const carregarTarefas = () => {
     const tarefas = localStorage.getItem('tarefas');
     return tarefas ? JSON.parse(tarefas) : [];
@@ -19,8 +18,11 @@ const adicionarTarefa = (nome) => {
     };
     tarefas.push(novaTarefa);
     salvarTarefas(tarefas);
+    renderizarTarefas();
 };
 const removerTarefa = (id) => {
+    if (!confirm('Deseja realmente excluir esta tarefa?'))
+        return;
     const tarefas = carregarTarefas();
     const tarefasAtualizadas = tarefas.filter(tarefa => tarefa.id !== id);
     salvarTarefas(tarefasAtualizadas);
@@ -46,19 +48,38 @@ const renderizarTarefas = () => {
     const listaTarefas = document.getElementById('taskList');
     listaTarefas.innerHTML = '';
     tarefas.forEach(tarefa => {
-        const listItem = document.createElement('li');
-        listItem.className = 'list-group-item';
-        listItem.textContent = tarefa.nome;
-        listItem.style.textDecoration = tarefa.status ? 'line-through' : 'none';
+        const listItem = document.createElement('tr');
+        listItem.innerHTML = `
+            <td class="text-center ${tarefa.status ? "text-decoration-line-through fst-italic" : ""} align-middle">${tarefa.id}</td>
+            <td class="text-center ${tarefa.status ? "text-decoration-line-through fst-italic" : ""} align-middle">${tarefa.nome}</td>
+            <td class="text-center ${tarefa.status ? "text-decoration-line-through fst-italic" : ""} align-middle">${tarefa.status ? 'Concluída' : 'Pendente'}</td>
+            <td class="d-flex justify-content-around">
+                <button id="btn-confirmar-${tarefa.id}" class="btn btn-success">
+                    <i class="fas fa-check"></i>
+                </button>
+                <button id="btn-edit-${tarefa.id}" class="btn btn-primary">
+                    <i class="fas fa-edit"></i>
+                </button>
+                <button id="btn-del-${tarefa.id}" class="btn btn-danger">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </td>`;
         listaTarefas.appendChild(listItem);
-        listItem.addEventListener('dblclick', () => {
-            const novoNome = prompt('Editar tarefa:', tarefa.nome);
+        const btnConfirmar = document.getElementById(`btn-confirmar-${tarefa.id}`);
+        btnConfirmar.addEventListener('click', () => {
+            alternarStatusTarefa(tarefa.id);
+            renderizarTarefas();
+        });
+        const btnEditar = document.getElementById(`btn-edit-${tarefa.id}`);
+        btnEditar.addEventListener('click', () => {
+            const novoNome = prompt('Informe o nome atualizado:', tarefa.nome);
             if (novoNome !== null)
                 editarTarefa(tarefa.id, novoNome);
             renderizarTarefas();
         });
-        listItem.addEventListener('click', () => {
-            alternarStatusTarefa(tarefa.id);
+        const btnExcluir = document.getElementById(`btn-del-${tarefa.id}`);
+        btnExcluir.addEventListener('click', () => {
+            removerTarefa(tarefa.id);
             renderizarTarefas();
         });
     });
@@ -71,5 +92,4 @@ formularioTarefa.addEventListener('submit', (e) => {
         inputTarefa.value = '';
     }
 });
-btnVisualizar.addEventListener('click', renderizarTarefas);
 renderizarTarefas();
--- src/ts/app.ts
+++ src/ts/app.ts
@@ -6,7 +6,6 @@ interface Tarefa {
 
 const formularioTarefa = document.getElementById('form') as HTMLFormElement;
 const inputTarefa = document.getElementById('task') as HTMLInputElement;
-// const btnVisualizar = document.getElementById('visualizar') as HTMLButtonElement;
 
 const carregarTarefas = (): Tarefa[] => {
     const tarefas = localStorage.getItem('tarefas');
@@ -27,9 +26,11 @@ const adicionarTarefa = (nome: string) => {
     };
     tarefas.push(novaTarefa);
     salvarTarefas(tarefas);
+    renderizarTarefas();
 };
 
 const removerTarefa = (id: number) => {
+    if (!confirm('Deseja realmente excluir esta tarefa?')) return;
     const tarefas = carregarTarefas();
     const tarefasAtualizadas = tarefas.filter(tarefa => tarefa.id !== id);
     salvarTarefas(tarefasAtualizadas);
@@ -53,29 +54,49 @@ const alternarStatusTarefa = (id: number) => {
     }
 };
 
-// const renderizarTarefas = () => {
-//     const tarefas = carregarTarefas();
-//     const listaTarefas = document.getElementById('taskList') as HTMLUListElement;
-//     listaTarefas.innerHTML = '';
-//     tarefas.forEach(tarefa => {
-//         const listItem = document.createElement('li');
-//         listItem.className = 'list-group-item';
-//         listItem.textContent = tarefa.nome;
-//         listItem.style.textDecoration = tarefa.status ? 'line-through' : 'none';
-//         listaTarefas.appendChild(listItem);
+const renderizarTarefas = () => {
+    const tarefas = carregarTarefas();
+    const listaTarefas = document.getElementById('taskList') as HTMLTableElement;
+    listaTarefas.innerHTML = '';
+    tarefas.forEach(tarefa => {
+        const listItem = document.createElement('tr');
+        listItem.innerHTML = `
+            <td class="text-center ${tarefa.status ? "text-decoration-line-through fst-italic" : ""} align-middle">${tarefa.id}</td>
+            <td class="text-center ${tarefa.status ? "text-decoration-line-through fst-italic" : ""} align-middle">${tarefa.nome}</td>
+            <td class="text-center ${tarefa.status ? "text-decoration-line-through fst-italic" : ""} align-middle">${tarefa.status ? 'Concluída' : 'Pendente'}</td>
+            <td class="d-flex justify-content-around">
+                <button id="btn-confirmar-${tarefa.id}" class="btn btn-success">
+                    <i class="fas fa-check"></i>
+                </button>
+                <button id="btn-edit-${tarefa.id}" class="btn btn-primary">
+                    <i class="fas fa-edit"></i>
+                </button>
+                <button id="btn-del-${tarefa.id}" class="btn btn-danger">
+                    <i class="fas fa-trash"></i>
+                </button>
+            </td>`
+        listaTarefas.appendChild(listItem);
 
-//         listItem.addEventListener('dblclick', () => {
-//             const novoNome = prompt('Editar tarefa:', tarefa.nome);
-//             if (novoNome !== null) editarTarefa(tarefa.id, novoNome);
-//             renderizarTarefas();
-//         });
+        const btnConfirmar = document.getElementById(`btn-confirmar-${tarefa.id}`) as HTMLButtonElement;
+        btnConfirmar.addEventListener('click', () => {
+            alternarStatusTarefa(tarefa.id);
+            renderizarTarefas();
+        });
 
-//         listItem.addEventListener('click', () => {
-//             alternarStatusTarefa(tarefa.id);
-//             renderizarTarefas();
-//         });
-//     });
-// };
+        const btnEditar = document.getElementById(`btn-edit-${tarefa.id}`) as HTMLButtonElement;
+        btnEditar.addEventListener('click', () => {
+            const novoNome = prompt('Informe o nome atualizado:', tarefa.nome);
+            if (novoNome !== null) editarTarefa(tarefa.id, novoNome);
+            renderizarTarefas();
+        });
+
+        const btnExcluir = document.getElementById(`btn-del-${tarefa.id}`) as HTMLButtonElement;
+        btnExcluir.addEventListener('click', () => {
+            removerTarefa(tarefa.id);
+            renderizarTarefas();
+        });
+    });
+}
 
 formularioTarefa.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -86,5 +107,4 @@ formularioTarefa.addEventListener('submit', (e) => {
     }
 });
 
-// btnVisualizar.addEventListener('click', renderizarTarefas);
-// renderizarTarefas();
+renderizarTarefas();
